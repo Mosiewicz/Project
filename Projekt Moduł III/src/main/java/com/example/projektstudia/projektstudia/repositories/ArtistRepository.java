7577d41b20b66f12c534cbd25a9f3b81ebab221d
Mosiewicz <71905073+Mosiewicz@users.noreply.github.com>
2020-09-25 23:46:25 +0200

    Add files via upload

---
+++ Projekt Moduł III/src/main/java/com/example/projektstudia/projektstudia/repositories/ArtistRepository.java
@@ -0,0 +1,7 @@
+package com.example.projektstudia.projektstudia.repositories;
+
+import com.example.projektstudia.projektstudia.model.Artist;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ArtistRepository extends CrudRepository<Artist,Long> {
+}
